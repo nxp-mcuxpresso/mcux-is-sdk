129749ac9eadddcc36a462b7dbd69a23d5b9b6e0
Amit Purohit <amit.purohit@nxp.com>
2024-11-13 23:25:17 -0700

    [MCUX-71811] cleaned the algorithm folder

--- algorithms/anomaly_detection/sources/feature_list.h
+++
@@ -1,9 +0,0 @@
-
-#define MAX_FEATURES 10
-extern struct FEATURE_LIST feature_list[MAX_FEATURES];
-void initialize_feature_list() ;
-bool get_feature_number(uint8_t *idx, sensor_t sensor, axis_t axis, feature_t feature) ;
-bool add_feature(uint8_t *idx, sensor_t sensor, axis_t axis, feature_t feature) ;
-bool delete_feature_by_index(uint8_t idx) ;
-bool delete_feature_by_attributes(sensor_t sensor, axis_t axis, feature_t feature) ;
-bool get_feature_attributes(uint8_t idx, sensor_t *sensor, axis_t *axis, feature_t *feature) ;
